c05435260dc8c48c46d86176d06832e313da8d1e
vigneshios <vigneshiosdev@gmail.com>
2018-02-20 11:10:50 +0530

    Check array element

--- CheckElementInArray/ViewController.swift
+++ CheckElementInArray/ViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var playersArray : [String] = Array()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        playersArray.append("M.S Dhoni")
+        playersArray.append("Virat Kohli")
+        playersArray.append("Dhawan")
+        playersArray.append("Ashwin")
+        
+        let isPlayerSelected = playersArray.contains { (str) -> Bool in
+            if str == "M.S Dhoni" {
+                return true
+            }
+            return false
+        }
+        
+        if isPlayerSelected == true {
+            print(" Yes, This player is the selected for the next tour")
+        }else {
+            print("This player name is not yet listed, please revisit tomo")
+        }
+        
     }
 
 
